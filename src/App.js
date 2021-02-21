3c50adf03186f357d75a2c5f2199d1c1ebd8027a
Arpit Gupta <agsinghgarg131@gmail.com>
2021-02-21 15:45:46 +0530

    made it!!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import InfiniteScroll from "react-infinite-scroller";
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      times: 1,
+    };
+  }
+
+  showItems() {
+    var items = [];
+    for (var i = 0; i < this.state.times; i++) {
+      items.push(
+        <div className="App" key={i}>
+          <h1>Hello World</h1>
+        </div>
+      );
+    }
+    return items;
+  }
+
+  loadMore() {
+    this.setState({ times: this.state.times + 20 });
+  }
+
+  render() {
+    return (
+      <InfiniteScroll pageStart={0} loadMore={this.loadMore.bind(this)} hasMore={true}>
+        {this.showItems()}
+      </InfiniteScroll>
+    );
+  }
 }
 
 export default App;
